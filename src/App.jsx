ede16924d080157dadf7258a74624c5c095507a4
Tom Byers <tmbyers1102@gmail.com>
2022-12-06 17:41:04 -0800

    initial epfm dash project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-Amber-500 w-full h-48'>
+      <h2>EPFM Dashboard</h2>
+    </div>
+  )
+}
+
+export default App
