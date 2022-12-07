761dd01d901f3a4188c0945f127074afb1a087ba
Tom Byers <tmbyers1102@gmail.com>
2022-12-06 17:48:45 -0800

    adjusted test home

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className='bg-Amber-500 w-full h-48'>
+    <div className='bg-amber-500 w-full h-48'>
       <h2>EPFM Dashboard</h2>
     </div>
   )
