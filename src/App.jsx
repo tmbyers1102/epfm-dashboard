9d7fe8653da89db3cbefd26b7e3f63931d62acda
Tom Byers <tmbyers1102@gmail.com>
2022-12-14 18:51:49 -0800

    trying adding basename to browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import './App.css'
 function App() {
 
   return (
-    <HashRouter>
+    <>
+    <BrowserRouter basename="/epfm-dashboard">
+      <Routes>
+        <Route path='/' element={<div>homeee</div>} />
+        <Route path='/testing' element={<div>TESTTTING</div>} />
+      </Routes>
+    </BrowserRouter>
+    {/* <HashRouter>
       <Routes>
         <Route path='/epfm-dashboard/' element={<div>homeee</div>} />
         <Route path='/epfm-dashboard/testing/' element={<div>TESTTTING</div>} />
       </Routes>
-    </HashRouter>
+    </HashRouter> */}
+    </>
 
     // <div className='bg-amber-500 w-full h-48'>
     //   <h2>EPFM Dashboard</h2>
