5da66d2df7f56bd64d20a7c00436e551c955ccb3
Tom Byers <tmbyers1102@gmail.com>
2022-12-14 18:41:22 -0800

    switched to hashrouter from browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/epfm-dashboard/' element={<div>homeee</div>} />
-        <Route path='/epfm-dashboard/testing' element={<div>TESTTTING</div>} />
+        <Route path='/epfm-dashboard/testing/' element={<div>TESTTTING</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
     // <div className='bg-amber-500 w-full h-48'>
     //   <h2>EPFM Dashboard</h2>
