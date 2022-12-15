0694e71390e3a05b86d26d10a6d8fa1871c99092
Tom Byers <tmbyers1102@gmail.com>
2022-12-14 22:37:08 -0800

    added new option for hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { HashRouter, BrowserRouter, Routes, Route, hashHistory } from 'react-router-dom'
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
+// import { HashRouter, BrowserRouter, Routes, Route, hashHistory } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <HashRouter history={hashHistory} basename="/epfm-dashboard">
-      <Routes>
-        <Route path='/' element={<div>homeee</div>} />
-        <Route path='/testing' element={<div>TESTTTING</div>} />
-      </Routes>
+    <HashRouter basename='/'>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
     </HashRouter>
   )
 }
+
+const Home = () => <div><h2>Home</h2></div>
+const About = () => <div><h2>About</h2></div>
 
 export default App
 
@@ -22,3 +32,11 @@ export default App
     <Route path='/testing' element={<div>TESTTTING</div>} />
   </Routes>
 </BrowserRouter> */}
+
+
+// <HashRouter history={hashHistory} basename="/epfm-dashboard">
+// <Routes>
+//   <Route path='/' element={<div>homeee</div>} />
+//   <Route path='/testing' element={<div>TESTTTING</div>} />
+// </Routes>
+// </HashRouter>
