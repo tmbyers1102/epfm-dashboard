1f7d63d46acd7bccb81648f6bb16258ac93401d3
Tom Byers <tmbyers1102@gmail.com>
2023-02-15 13:03:53 -0800

    switching back to local api endpoints

--- src/AppTwo.jsx
+++ src/AppTwo.jsx
@@ -30,8 +30,8 @@ function AppTwo() {
 
     const getData = async () => {
         // clients
-        // const clientResponse = await fetch('http://127.0.0.1:8000/api/clients/')
-        const clientResponse = await fetch('https://tmbyers3310.pythonanywhere.com/api/clients/')
+        // const clientResponse = await fetch('https://tmbyers3310.pythonanywhere.com/api/clients/')
+        const clientResponse = await fetch('http://127.0.0.1:8000/api/clients/')
         const clientData = await clientResponse.json()
         setClients(clientData)
         // console.log(clientData)
